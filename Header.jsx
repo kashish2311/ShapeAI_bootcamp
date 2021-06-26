cf6821dab2a90c1b1b69ba1a04d1d1051370dce5
kashish2311 <86370529+kashish2311@users.noreply.github.com>
2021-06-26 23:44:43 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Header(){
+    return(
+        <header>
+            <h1>SHAPEAI Bootcamps</h1>
+        </header>
+        
+       
+    );
+}
+
+export default Header;
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "/.componrnts/App";
+ReactDOM.render(
+    <App />,
+  document.getElementById("root")
+);
